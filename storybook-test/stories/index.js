72dda34a3495e28c6fd60932bdc7fb6f9e0401d4
松村和輝 <k.matsumura0314@gmail.com>
2018-02-05 11:08:17 +0900

    prettify

--- storybook-test/stories/index.js
+++ storybook-test/stories/index.js
@@ -5,14 +5,8 @@ import Button from "../components/Button";
 
 storiesOf("Button", module)
   .add("with text", () => (
-    <Button
-      onClick={action("clicked")}
-      >
-        Hello Button
-    </Button>
+    <Button onClick={action("clicked")}>Hello Button</Button>
   ))
   .add("with some emoji", () => (
     <Button onClick={action("clicked")}>😀 😎 👍 💯</Button>
-  ))
-  ;
-
+  ));
