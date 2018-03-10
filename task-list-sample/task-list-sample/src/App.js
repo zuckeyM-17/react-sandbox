14a3aaf280dd13679fb6f47fd15d38c0a90a2836
松村和輝 <k.matsumura0314@gmail.com>
2018-03-10 23:52:35 +0900

    init task list sample

---
+++ task-list-sample/task-list-sample/src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <ul>
+        <li>hoge</li>
+        <li>fuga</li>
+        <li>piyo</li>
+      </ul>
+    );
+  }
+}
+
+export default App;
